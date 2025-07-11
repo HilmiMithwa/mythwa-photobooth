63c3279fe80bb4f6d8b00bf38f2931aff192d80c
Hilmi Mithwa <hilmimithwa@gmail.com>
2025-07-11 22:05:15 +0700

    logic added at mainmenu

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 
 
 export default function MainMenu() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [dropDown, setDropDown] = useState(false);
+
+  const handleDropDown= () => {
+    setDropDown(prev => !prev);
+  };
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (!e.target.closest('#social-media-dropdown')) {
+        setDropDown(false);
+      }
+    };
+    if (dropDown) {
+      document.addEventListener('mousedown', handleClick);
+    }
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, [dropDown]);
 
   return (
     <>
@@ -14,8 +33,20 @@ export default function MainMenu() {
           <nav className='space-x-7'>
             <a>Home</a>
             <a>About</a>
-            <a>Social Media</a>
-            <a>Your Recent photo</a>
+            <div id='social-media-dropdown' className='relative inline-block'>
+              <button id='social-media' onClick={handleDropDown}>Social Media</button>
+              {dropDown && (
+                <div className='absolute left-0 top-full mt-2 bg-white shadow-lg border rounded w-48 z-10'>
+                  <ul className='flex flex-col text-left p-2 '>
+                    <li><a href='https://www.instagram.com/hilmimthwaa__/?next=%2F'>hilmimthwaa__</a></li>
+                    <li><a>hilmimithwa@gmail.com</a></li>
+                    <li><a>HilmiMithwa</a></li>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <a>Your Recent Photos</a>
+
           </nav>
         </header>
         <main className='flex flex-col items-center justify-center min-h-[60vh] '>
