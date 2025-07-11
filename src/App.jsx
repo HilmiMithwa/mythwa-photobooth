225f680dd080837d4a877c650766b81cc0eb8e77
Hilmi Mithwa <hilmimithwa@gmail.com>
2025-07-11 18:19:59 +0700

    Finishing the html of mainmenu, css later

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import MainMenu from './pages/MainMenu'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import SelectBorder from './pages/SelectBorder'
 
 export default function App() {
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainMenu />}/>
+        <Route path='/selectborder' element={<SelectBorder />} />
+
+      </Routes>
+    </Router>
+
   )
 }
--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-import { useState } from 'react'
+import Navbar from '../components/Navbar'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function MainMenu() {
+  const navigate = useNavigate()
+
   return (
-    <div>MainMenu</div>
+    <>
+    <div className='font-roboto'>
+      <header className='flex justify-between items-center px-4 py-2 m-5'>
+        <h1>Mythwa Photobooth</h1>
+          <nav className='space-x-7'>
+            <a>Home</a>
+            <a>About</a>
+            <a>Social Media</a>
+            <a>Your Recent photo</a>
+          </nav>
+        </header>
+        <main className='flex flex-col items-center justify-center min-h-[60vh] '>
+          <div id='welcoming-screen' className='text-center '>
+            <h1>Welcome!</h1>
+            <p>Capture the Moment, Share the Joy!</p>
+            <div id='start-button' className='bg-color'>
+              <button onClick={() => navigate('/selectborder')}>Start!</button>
+            </div>
+          </div>
+
+
+        </main>
+      </div>
+      
+    </>
   )
 }
