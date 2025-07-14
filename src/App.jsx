0430c0739522bf269bb646a0e784bbd9aa00156b
Hilmi Mithwa <hilmimithwa@gmail.com>
2025-07-14 12:52:02 +0700

    Finishing frame2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MainMenu from './pages/MainMenu'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SelectBorder from './pages/SelectBorder'
+import Frame2 from './pages/border/Frame2'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<MainMenu />}/>
         <Route path='/selectborder' element={<SelectBorder />} />
+        <Route path='/frame2' element={<Frame2 />} />
 
       </Routes>
     </Router>
