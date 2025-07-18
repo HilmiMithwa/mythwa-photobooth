3162d562c38951c9746fb9ef34ff24e8c3ddd85e
Hilmi Mithwa <hilmimithwa@gmail.com>
2025-07-18 12:50:03 +0700

    Finish the mainmenu, responsive later

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import * as React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 import SNAPZY from '../images/SNAPZY.png';
 import randomPerson from '../images/photos.png';
 import BackgroundMainmenu from '../images/BackgroundMainmenu.png'
-
-import { useNavigate } from 'react-router-dom';
-
-
+import photos2 from '../images/photos2.png'
+import photos3 from '../images/photos3.png'
 
 
 export default function MainMenu() {
   const navigate = useNavigate()
+  const images = [randomPerson, photos2, photos3]
 
   return (
-    <div className='w-screen h-screen bg-cover bg-center' style={{backgroundImage: `url(${BackgroundMainmenu})` }}>
+    <div className='w-screen h-screen bg-cover bg-center flex flex-col' style={{backgroundImage: `url(${BackgroundMainmenu})` }}>
       <nav className='flex justify-between items-center font-manrope bg-white px-12 py-10'>
 
         <div className='flex items-center space-x-100'>
@@ -27,23 +38,39 @@ export default function MainMenu() {
         <div>
           <h1 className='bg-darkenBlue text-white text-lg px-20 py-5 text-[25px] mr-45'>Your Recent Photos</h1>
         </div>
-
       </nav>
 
-      <div className='bg-neonPink text-white w-[1211px] h-[640px] rounded-3xl px-20 py-20 flex justify-between items-center'>
-        <div className='flex flex-col gap-5 w-2/3'>
-          <h1 className='font-luckiest text-[60px]'>Welcome to<br/> snapzy!</h1>
-          <h2 className='font-loved text-[50px]'>Capture the moment, share the joy!</h2>
-          <h3 className='font-manrope text-[25px]'>A simple way to capture a precious moment using <br/> portable booth</h3>
-          <button className='font-londrina bg-darkNeonPink rounded-[25px] text-[45px]'>Start!</button>
+      <div className='flex flex-1 justify-center items-center'>
+        <div className='bg-neonPink text-white w-[1211px] h-[640px] rounded-3xl px-20 py-20 flex justify-between items-center'> 
+          <div className='flex flex-col gap-5 w-2/3'>
+            <h1 className='font-luckiest text-[60px]'>Welcome to<br/> snapzy!</h1>
+            <h2 className='font-loved text-[50px]'>Capture the moment, share the joy!</h2>
+            <h3 className='font-manrope text-[25px]'>A simple way to capture a precious moment using <br/> portable booth</h3>
+            <button className='font-londrina bg-darkNeonPink rounded-[25px] text-[45px]'>Start!</button>
+          </div>
+
+          <div id='carousel-container' className=''>
+            <Carousel
+              plugins={[
+                Autoplay({
+                  delay: 2000,
+                })
+              ]}
+            >
+              <CarouselContent className='w-[345px] h-[467px]'>
+                {images.map((img, index) => (
+                  <CarouselItem key={index} className='basis-full'>
+                    <Card className='custom-dashed-4side border-none'>
+                      <CardContent className='flex aspect-square items-center justify-center p-6'>
+                        <img src={img} alt={`carousel-${index}`} className='object-cover w-full h-full rounded-xl' />
+                      </CardContent>
+                    </Card>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              </Carousel>
+          </div>
         </div>
-
-        <div id='img-container' className=''>
-          <img src={randomPerson} />
-        </div>
-        
-
-
       </div>
     </div>
   )
